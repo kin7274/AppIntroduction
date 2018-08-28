b0fbe07e4628611b20c18bfd54c5b86899a31757
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-08-28 16:01:50 +0900

    메뉴 1,4번 인텐트 구현
    메뉴 1,4번 인텐트 구현

---
+++ app/src/main/java/com/example/administrator/appintroduction/IntroActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.appintroduction;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+
+    }
+}
--- app/src/main/java/com/example/administrator/appintroduction/MainActivity.java
+++ app/src/main/java/com/example/administrator/appintroduction/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.administrator.appintroduction;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    // 메뉴 4개(텍스트뷰로 구현)
     TextView menu1, menu2, menu3, menu4;
 
     @Override
@@ -21,37 +24,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setSupportActionBar(mytoolbar);
         getSupportActionBar().setTitle("");
 
-        menu1 = (TextView) findViewById(R.id.menu1);
-        menu2 = (TextView) findViewById(R.id.menu2);
-        menu3 = (TextView) findViewById(R.id.menu3);
-        menu4 = (TextView) findViewById(R.id.menu4);
+        menu1 = ( TextView ) findViewById(R.id.menu1);
+        menu2 = ( TextView ) findViewById(R.id.menu2);
+        menu3 = ( TextView ) findViewById(R.id.menu3);
+        menu4 = ( TextView ) findViewById(R.id.menu4);
         menu1.setOnClickListener(this);
         menu2.setOnClickListener(this);
         menu3.setOnClickListener(this);
         menu4.setOnClickListener(this);
     }
 
-    public void onClick(View v){
-        switch(v.getId()){
+    // 메뉴 4개(텍스트뷰) 클릭 이벤트
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.menu1:
                 // 1번 메뉴 : 당뇨그루에 대해
-                Toast.makeText(this, "1번 메뉴 클릭", Toast.LENGTH_LONG).show();
-
+                // 액티비티 넘겨
+//                Toast.makeText(this, "1번 메뉴 클릭", Toast.LENGTH_LONG).show();
+                Intent IntroIntent = new Intent(MainActivity.this, IntroActivity.class);
+                startActivity(IntroIntent);
                 break;
             case R.id.menu2:
                 // 2번 메뉴 : 앱버전 정보
+                // 다이얼로그
                 Toast.makeText(this, "2번 메뉴 클릭", Toast.LENGTH_LONG).show();
-
                 break;
             case R.id.menu3:
                 // 3번 메뉴 : 개발자 정보
+                // 리사이클러뷰
+                // 어댑터 형식을 생각하자!
                 Toast.makeText(this, "3번 메뉴 클릭", Toast.LENGTH_LONG).show();
 
                 break;
             case R.id.menu4:
                 // 4번 메뉴 : Change log
-                Toast.makeText(this, "4번 메뉴 클릭", Toast.LENGTH_LONG).show();
-
+                // 변경 내용
+                // 액티비티 넘겨
+//                Toast.makeText(this, "4번 메뉴 클릭", Toast.LENGTH_LONG).show();
+                Intent changeLogIntent = new Intent(MainActivity.this, ChangeLogActivity.class);
+                startActivity(changeLogIntent);
                 break;
         }
     }
